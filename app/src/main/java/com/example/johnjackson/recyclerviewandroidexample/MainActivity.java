e1804e36c357d9b9b4bb42069cdd124d91c34d3e
jgjio <johngjackson0@gmail.com>
2018-05-20 14:40:44 -0400

    Add a Linear Layout Manager for Recycler View

--- app/src/main/java/com/example/johnjackson/recyclerviewandroidexample/MainActivity.java
+++ app/src/main/java/com/example/johnjackson/recyclerviewandroidexample/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.johnjackson.recyclerviewandroidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_ITEMS = 100;
+    //TODO Implement RecyclerViewTextAdapter
+    //private RecyclerViewTextAdapter mTextRecyclerViewAdapter;
+    private RecyclerView mTextList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextList = (RecyclerView) findViewById(R.id.recycler_view_items);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mTextList.setLayoutManager(layoutManager);
+        mTextList.setHasFixedSize(true);
+        //todo connect layout manager to recyclerview adapter
+        //mTextRecyclerViewAdapter = new RecyclerViewTextAdapter(NUM_ITEMS);
+        //mTextList.setAdapter(mAdapter);
+
     }
 }
